c74dd76ce18d69c13eb2da188f0ebb55b643a7be
Neo <guangy2@illinois.edu>
2023-10-31 04:51:41 -0500

    format

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,28 +1,32 @@
-return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.colorscheme.gruvbox-nvim" },
-  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
-  -- { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
-  -- { import = "astrocommunity.colorscheme.nightfox-nvim" },
-  -- { import = "astrocommunity.colorscheme.dracula-nvim" },
-  -- { import = "astrocommunity.colorscheme.vscode-nvim" },
-  -- { import = "astrocommunity.colorscheme.fluormachine-nvim" },
-  -- { import = "astrocommunity.colorscheme.everforest" },
-  -- { import = "astrocommunity.colorscheme.kanagawa-nvim" },
-  -- { import = "astrocommunity.colorscheme.nordic-nvim" },
-  -- { import = "astrocommunity.colorscheme.nvim-juliana" },
-  -- { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
-  -- { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
-  -- { import = "astrocommunity.colorscheme.sonokai" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.completion.copilot-lua"},
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim"},
-}
+return { -- Add the community repository of plugin specifications
+"AstroNvim/astrocommunity", -- example of importing a plugin, comment out to use it or add your own
+-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+-- { import = "astrocommunity.colorscheme.catppuccin" },
+-- { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+{
+    import = "astrocommunity.colorscheme.tokyonight-nvim"
+}, -- { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+-- { import = "astrocommunity.colorscheme.nightfox-nvim" },
+-- { import = "astrocommunity.colorscheme.dracula-nvim" },
+-- { import = "astrocommunity.colorscheme.vscode-nvim" },
+-- { import = "astrocommunity.colorscheme.fluormachine-nvim" },
+-- { import = "astrocommunity.colorscheme.everforest" },
+-- { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+-- { import = "astrocommunity.colorscheme.nordic-nvim" },
+-- { import = "astrocommunity.colorscheme.nvim-juliana" },
+-- { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+-- { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
+-- { import = "astrocommunity.colorscheme.sonokai" },
+{
+    import = "astrocommunity.pack.python"
+}, {
+    import = "astrocommunity.pack.cpp"
+}, {
+    import = "astrocommunity.pack.markdown"
+}, {
+    import = "astrocommunity.pack.lua"
+}, {
+    import = "astrocommunity.completion.copilot-lua"
+}, {
+    import = "astrocommunity.markdown-and-latex.markdown-preview-nvim"
+}}
